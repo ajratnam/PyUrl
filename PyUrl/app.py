c8897cf278e24ca91a1e4018f4586e80f12ad646
Ajay Ratnam <ajratnam.dev@gmail.com>
2022-08-13 17:47:33 +0530

    Linked the HTML Files to the URLs

--- PyUrl/app.py
+++ PyUrl/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def home():
+    return render_template('home.html')
+
+
+@app.get('/shorten')
+def shorten_get():
+    return render_template('shorten.html')
+
+
+@app.post('/shorten')
+def shorten_post():
+    url = request.form['url']
+    if not url:
+        return 'No URL provided'
+    return render_template('shorten.html', url=url, surl='shortened_link')
 
 
 if __name__ == '__main__':
