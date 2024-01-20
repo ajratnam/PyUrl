2752ce75e384825d44206a9a1b8874866b72f47f
Ajay Ratnam <ajratnam.dev@gmail.com>
2024-01-20 09:17:52 +0530

    create endpoints

---
+++ backend/app.py
@@ -0,0 +1,44 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, HTTPException, Depends
+from starlette.responses import RedirectResponse
+
+from backend import models
+from backend.crud import generate_random_code, create_url, get_url
+from backend.database import engine, Session
+from backend.schemas import ShortUrl, RequestUrl
+
+
+@asynccontextmanager
+async def connect_db(_: FastAPI):
+    async with engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
+    yield
+    await engine.dispose()
+
+
+app = FastAPI(lifespan=connect_db)
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/shorten", response_model=ShortUrl)
+async def shorten_url(request: RequestUrl, db: Session = Depends(get_db)):
+    request.code = request.code or await generate_random_code(db, 6)
+    try:
+        return await create_url(db, request)
+    except ValueError:
+        raise HTTPException(status_code=409, detail="Code already exists")
+
+
+@app.get("/{code}")
+async def redirect(code: str, db: Session = Depends(get_db)):
+    if short_url := await get_url(db, code):
+        return RedirectResponse(url=short_url.origin)
+    raise HTTPException(status_code=404, detail="URL not found")
