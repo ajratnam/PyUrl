4d19de26736f2e3aea3639c08122a4ce4e43f621
Ajay Ratnam <ajratnam.dev@gmail.com>
2024-01-20 10:16:07 +0530

    Wrap in package

--- PyUrl/backend/app.py
+++ PyUrl/backend/app.py
@@ -3,10 +3,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException, Depends
 from starlette.responses import RedirectResponse
 
-from backend import models
-from backend.crud import generate_random_code, create_url, get_url
-from backend.database import engine, Session
-from backend.schemas import ShortUrl, RequestUrl
+from PyUrl.backend import models
+from PyUrl.backend.crud import generate_random_code, create_url, get_url
+from PyUrl.backend.database import engine, Session
+from PyUrl.backend.schemas import ShortUrl, RequestUrl
 
 
 @asynccontextmanager
@@ -20,12 +20,12 @@ async def connect_db(_: FastAPI):
 app = FastAPI(lifespan=connect_db)
 
 
-def get_db():
+async def get_db():
     db = Session()
     try:
         yield db
     finally:
-        db.close()
+        await db.close()
 
 
 @app.post("/shorten", response_model=ShortUrl)
